17abb895bc1521ac1be6bbb39c8dbc8ad0f8e314
Julian Biebelberg <jbiebelberg@gmail.com>
2016-05-24 08:46:53 -0400

    most recent update

--- jquery.js
+++ jquery.js
@@ -9,19 +9,66 @@ function reset(){
 	$('#attackMessage').delay(2000).fadeOut();
 };
 
-
-var capHealt = 1100;
+//Variables
+var capHealth = 1100;
 var ironmanHealth = 950;
 var deadpoolHealth = 850;
 var ultronHealth1 = 1000;
 var ultronHealth2 = 1000;
-var ultronHealth3 = 1000;	
+var ultronHealth3 = 1000;
+
+
+// Boss Attack Sequence
+function bossAttackSequence(){
+
+$(document).ready(function(){
+
+var heroes = ["cap", "ironman", "deadpool"];
+
+var choice = heroes[Math.floor(Math.random()*3)];
+
+var ultronAttack = Math.floor(Math.random()*(300-200+1)+200);
+
+var ultronCrit = Math.floor(Math.random()*100);
+
+if(ultronCrit <= 15){
+		ultronAttack = Math.floor(ultronAttack + .25*ultronAttack);
+	}
+
+if(choice=="cap"){
+	$('#attackMessage').show(1000, function(){
+	$('#attackMessage').html('Ultron attacked Captain America for ' + ultronAttack + ' damage');
+	reset();
+	return;
+	});
+	
+}
+
+if(choice=="ironman"){
+	$('#attackMessage').show(1000, function(){
+	$('#attackMessage').html('Ultron attacked Ironman for ' + ultronAttack + ' damage');
+	reset();
+	return;
+	});
+	
+}
+
+if(choice=="deadpool"){
+	$('#attackMessage').show(1000, function(){
+	$('#attackMessage').html('Ultron attacked Deadpool for ' + ultronAttack + ' damage');
+	reset();
+	return;
+	});
+	
+}
+
+});
+
+};	
 
 
 //Hero MainAttack Sequence
 function heroAttackSequence(){
-
-	
 
 	//Critical Hit Percentages
 	var capCrit =  Math.floor(Math.random()*100);
@@ -35,15 +82,15 @@ function heroAttackSequence(){
 	
 
 	// Result of Crits
-	if(capCrit >= 25){
+	if(capCrit <= 25){
 		capAttack = Math.floor(capAttack + .25*capAttack);
 	}
 
-	if(ironmanCrit >= 20){
+	if(ironmanCrit <= 20){
 		ironmanAttack = Math.floor(ironmanAttack + .25*ironmanAttack);
 	}
 
-	if(deadpoolCrit >= 25){
+	if(deadpoolCrit <= 25){
 		deadpoolAttack = Math.floor(deadpoolAttack + .25*deadpoolAttack);
 	}
 
@@ -60,10 +107,11 @@ function heroAttackSequence(){
 	});
 
 	$('.boss').on('click', function(){
+		
 		$('#attackMessage').show();
 		$('#attackMessage').html('Captain America attacked for ' + capAttack + ' damage');
 		reset();
-		$('#cap').off('click');
+		bossAttackSequence();
 		
 	});
 });
@@ -83,7 +131,7 @@ function heroAttackSequence(){
 		$('#attackMessage').show();
 		$('#attackMessage').html('Ironman attacked for ' + ironmanAttack + ' damage');
 		reset();
-		$('#ironman').off('click');
+		bossAttackSequence();
 		
 	});
 });
@@ -103,7 +151,7 @@ function heroAttackSequence(){
 		$('#attackMessage').show();
 		$('#attackMessage').html('Deadpool attacked for ' + deadpoolAttack + ' damage');
 		reset();
-		$('#deadpool').off('click');
+		bossAttackSequence();
 		
 	});
 });
